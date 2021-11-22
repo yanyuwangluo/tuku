ad85e59f27b97111bba37636454ca8fd45b96941
yanyuwangluo <1915241107@qq.com>
2021-11-22 23:40:06 +0800

    Create mogai.sh

---
+++ mogai/mogai.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+ echo -e "\e[36m
+   ▄▄▄▄       ██                         ▄▄▄▄                                   
+  ██▀▀██      ▀▀                         ▀▀██                                   
+ ██    ██   ████     ██▄████▄   ▄███▄██    ██       ▄████▄   ██▄████▄   ▄███▄██ 
+ ██    ██     ██     ██▀   ██  ██▀  ▀██    ██      ██▀  ▀██  ██▀   ██  ██▀  ▀██ 
+ ██    ██     ██     ██    ██  ██    ██    ██      ██    ██  ██    ██  ██    ██ 
+  ██▄▄██▀  ▄▄▄██▄▄▄  ██    ██  ▀██▄▄███    ██▄▄▄   ▀██▄▄██▀  ██    ██  ▀██▄▄███ 
+   ▀▀▀██   ▀▀▀▀▀▀▀▀  ▀▀    ▀▀   ▄▀▀▀ ██     ▀▀▀▀     ▀▀▀▀    ▀▀    ▀▀   ▄▀▀▀ ██ 
+       ▀                        ▀████▀▀                                 ▀████▀▀
+-----------------------------烟雨阁出品一键-------------------------------------
+-----------------------------原作者：风之凌殇-----------------------------------
+\e[0m\n"
+sleep 5
+echo 开始执行替换魔改互助文件
+wget -nv https://raw.githubusercontent.com/yanyuwangluo/tuku/main/mogai/task_before.sh -O ~/ql/config/task_before.sh  
+
+wget https://raw.githubusercontent.com/yanyuwangluo/tuku/main/mogai/code.sh -O ~/ql/config/code.sh
+
+wget https://raw.githubusercontent.com/yanyuwangluo/tuku/main/mogai/jdCookie.js -O ~/ql/config/jdCookie.js
+
+echo 下载完成
+
+echo 正在替换jdcookie.js
+
+sleep 5
+
+cp -rf ~/ql/config/jdCookie.js ~/ql/scripts
+
+echo 执行结束告辞。
